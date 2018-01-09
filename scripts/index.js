c51e371a75ff847864cd791933e82385fba30caf
JinXin1995 <jinxin807507011@outlook.com>
2018-01-09 16:22:12 +0800

    添加底部输入按钮

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,50 @@
 var vm = new Vue({
     el: ".content",
     data: {
-        board: [],
-        selected_unit: null,
-        selected_btn: null
+        board: initBoard,
+        selected_unit: null,    //选中的单元格
+        selected_btn: null, //选中的按钮
+        selected_num: null  //选中的数字
     },
-    mounted: function () {
-        this.board = initBoard();
-    },
+    // mounted: function () {
+    //     this.board = initBoard();
+    // },
     methods: {
         clickUnit: function (row, column) {
-            if (this.selected_unit && row === this.selected_unit.row && column === this.selected_unit.column) {
+            if (this.selected_btn) {
+                fillNum(this.selected_btn, row, column);
+            } else if (this.selected_unit && row === this.selected_unit.row && column === this.selected_unit.column) {
                 this.selected_unit = null;
+                this.selected_num = null;
             } else {
                 this.selected_unit = {
                     row: row,
                     column: column
                 };
+                if (this.board[row][column].num != null) {
+                    this.selected_num = this.board[row][column].num;
+                }
             }
         },
-        clickNumBtn: function (num) {
-            this.selected_btn = num;
+        clickBtn: function (option) {
+            if (this.selected_unit) {
+                fillNum(option, this.selected_unit.row, this.selected_unit.column);
+            } else if (this.selected_btn === option) {
+                this.selected_btn = null;
+                this.selected_num = null;
+            } else {
+                this.selected_btn = option;
+                if (typeof option === 'number') {
+                    this.selected_num = option;
+                } else {
+                    this.selected_num = null;
+                }
+            }
         },
         disSelect: function () {
             this.selected_unit = null;
             this.selected_btn = null;
+            this.selected_num = null;
         }
     }
 });
--- scripts/option.js
+++ scripts/option.js
@@ -1,4 +1,4 @@
-function initBoard() {
+var initBoard = (function () {
     var board = [];
     for (var i = 0; i < 9; i++) {
         board[i] = [];
@@ -10,4 +10,19 @@ function initBoard() {
         }
     }
     return board;
+})();
+
+function fillNum(option, row, column) {
+    //判断待填充的单元是否为本局游戏的题目
+    if (!vm.board[row][column].default) {
+        var num = option;
+        if (option === 'X') {
+            num = null;
+        }
+        if (vm.board[row][column].num === num) {
+            vm.board[row][column].num = null;   //如果要输入的数字与现有的相同，则改为清空单元格
+        } else {
+            vm.board[row][column].num = num;
+        }
+    }
 }
